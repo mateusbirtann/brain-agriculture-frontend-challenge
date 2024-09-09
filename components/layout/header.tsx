d93ef0b897adc084d7473f96cd1efaae5e49da93
MateusBirtan <contatobirtan@gmail.com>
2024-09-09 13:18:34 -0300

    refactor: Update header component to use 'md' instead of 'lg' for hiding mobile sidebar

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="sticky inset-x-0 top-0 w-full">
       <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
-        <div className={cn('block lg:!hidden')}>
+        <div className={cn('block md:!hidden')}>
           <MobileSidebar />
         </div>
       </nav>
